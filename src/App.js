840bded63db6b3793b655d682362f33c5dfa2bf9
JeongSeongHyun <tjdgus7562@naver.com>
2023-11-23 09:35:41 +0900

    폴더 구조 변경 및 scss를 통한 css 적용해보기 / 일기장 기본 프레임 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import "./assets/css/style.css"; //index.html에서 하는것과 차이는??
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <div className="container">
+                <div>오늘의 일기</div>
+                <form>
+                    <input className="title" />
+                    <textarea className="content" />
+                    <div className="score-container">
+                        <span>오늘의 감정점수 : </span>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                    </div>
+                    <button className="save">일기 저장하기</button>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default App;
